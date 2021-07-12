7cfa13a76c8a92ff6320c3f3bf18c36731eae7d6
Monika Kaploniak <monikakaploniak@Monikas-MacBook-Pro.local>
2021-07-12 14:51:35 +0100

    created a navbar

--- src/components/NavHeader.jsx
+++ src/components/NavHeader.jsx
@@ -5,10 +5,9 @@ import Nav from "react-bootstrap/Nav";
 const NavHeader = () => {
   return (
     <>
-      <Navbar bg="dark" variant="dark">
+      <Navbar bg="light" variant="light">
         <Nav className="mr-auto">
           <Nav.Link href="/home">HOME</Nav.Link>
-          <Nav.Link href="/about">ABOUT</Nav.Link>
           <Nav.Link href="/reviews">REVIEWS</Nav.Link>
           <Nav.Link href="/categories">CATEGORIES</Nav.Link>
         </Nav>
