2105de13c3d2cb36509881b1c324b91a10bdfd1e
Monika Kaploniak <monikakaploniak@Monikas-MacBook-Pro.local>
2021-07-12 15:32:52 +0100

    created home component with All reviews listed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { getReviews } from "../utils/api";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return <div></div>;
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    getReviews().then((reviewsFromApi) => {
+      setReviews(reviewsFromApi);
+    });
+  }, []);
+
+  return (
+    <main className="Home">
+      <h1>Reviews:</h1>
+      {reviews.map((review) => {
+        return (
+          <li key={review.review_id}>
+            <Link to={`/reviews/${review.review_id}`} className="Reviews_link">
+              <h2>{review.title}</h2>
+              <img
+                className="Review_img_url"
+                src={review.review_img_url}
+                alt={""}
+              ></img>
+            </Link>
+          </li>
+        );
+      })}
+    </main>
+  );
 };
 
 export default Home;
--- src/utils/api.js
+++ src/utils/api.js
@@ -4,8 +4,13 @@ const reviewsApi = axios.create({
   baseURL: "https://nc-games-monika.herokuapp.com/api",
 });
 
-export const getReviews = () => {
-  reviewsApi.get("/reviews").then((response) => {
-    console.log(response.data);
-  });
+// export const getReviews = () => {
+//   reviewsApi.get("/reviews").then((response) => {
+//     console.log(response.data);
+//   });
+// };
+
+export const getReviews = async () => {
+  const { data } = await reviewsApi.get("/reviews");
+  return data.reviews;
 };
