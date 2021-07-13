e3b82603f0c80e9432f311dd9638f0d120bc4b79
Monika Kaploniak <monikakaploniak@Monikas-MacBook-Pro.local>
2021-07-13 12:21:50 +0100

    created Comments component

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { getCommentsByReviewId } from "../utils/api";
 
 const Comments = () => {
-  return <div></div>;
+  const [comment, setComment] = useState({});
+  const { review_id } = useParams();
+
+  useEffect(() => {
+    getCommentsByReviewId(review_id).then((commentsFromApi) => {
+      setComment(commentsFromApi);
+    });
+  }, [review_id]);
+
+  return (
+    <main className="Comments">
+      <h1>{comment.body}</h1>
+      <p>Author: {comment.author}</p>
+      <p>{comment.created_at}</p>
+      <p>👍 {comment.votes}</p>
+    </main>
+  );
 };
 
 export default Comments;
