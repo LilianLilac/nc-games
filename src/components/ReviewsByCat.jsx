92a64d1edb0b38014c3806b5ef0b0ffb2a4b085f
Monika Kaploniak <monikakaploniak@Monikas-MacBook-Pro.local>
2021-07-15 22:16:17 +0100

    refactor for getting reviews by category

--- src/components/ReviewsByCat.jsx
+++ src/components/ReviewsByCat.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getReviewsByCat } from "../utils/api";
+import Card from "react-bootstrap/Card";
 
 const ReviewsByCat = () => {
   const [reviews, setReviews] = useState([]);
@@ -14,19 +15,27 @@ const ReviewsByCat = () => {
   console.log(category);
 
   return (
-    <div className="Reviews">
-      <h2>{`${category} reviews`}</h2>
-      <ul>
-        {reviews.map((review) => {
-          return (
-            <li key={review.review_id}>
-              <h3>{review.title}</h3>
-              <p>{review.review_body}</p>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
+    <main className="Reviews_by_cat">
+      <h1>{`${category} reviews`}</h1>
+      {reviews.map((review) => {
+        return (
+          <p key={review.review_id} className="Reviews_list">
+            <Card style={{ maxwidth: "10rem" }}>
+              <Card.Body>
+                <Card.Title>{review.title}</Card.Title>
+                <Card.Text>{review.review_body}</Card.Text>
+                <Card.Text>Author: {review.owner}</Card.Text>
+              </Card.Body>
+              <Card.Img
+                variant="top"
+                src={review.review_img_url}
+                className="Card_img"
+              />
+            </Card>
+          </p>
+        );
+      })}
+    </main>
   );
 };
 
