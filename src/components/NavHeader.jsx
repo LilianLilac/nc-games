d7d3b50a29981d8b658e9a786db19b3e56153083
Monika Kaploniak <monikakaploniak@Monikas-MacBook-Pro.local>
2021-07-19 12:04:50 +0100

    refactored navbar to scroll when collapsed

--- src/components/NavHeader.jsx
+++ src/components/NavHeader.jsx
@@ -7,41 +7,49 @@ import UserProfile from "./UserProfile";
 const NavHeader = () => {
   return (
     <>
-      <Navbar sticky="top" bg="light" variant="light">
+      <Navbar sticky="top" bg="light" justify variant="light" expand="lg">
         <Navbar.Brand href="/home">NC Games</Navbar.Brand>
-        <Nav className="mr-auto">
-          <NavDropdown title="CATEGORIES" id="collasible-nav-dropdown">
-            <NavDropdown.Item href="/reviews/category/strategy">
-              Strategy
-            </NavDropdown.Item>
-            <NavDropdown.Item href="/reviews/category/hidden-roles">
-              Hidden-roles
-            </NavDropdown.Item>
-            <NavDropdown.Item href="/reviews/category/dexterity">
-              Dexterity
-            </NavDropdown.Item>
-            <NavDropdown.Item href="/reviews/category/push-your-luck">
-              Push-your-luck
-            </NavDropdown.Item>
-            <NavDropdown.Item href="/reviews/category/roll-and-write">
-              Roll-and-write
-            </NavDropdown.Item>
-            <NavDropdown.Item href="/reviews/category/deck-building">
-              Deck-building
-            </NavDropdown.Item>
-            <NavDropdown.Item href="/reviews/category/engine-building">
-              Engine-building
-            </NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="/categories">
-              See All Categories
-            </NavDropdown.Item>
-          </NavDropdown>
-          {/* <Navbar.Text>Signed in as:</Navbar.Text> */}
-          <Nav.Link eventKey="disabled">
-            <UserProfile />
-          </Nav.Link>
-        </Nav>
+        {/* <Nav className="mr-auto"> */}
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="mr-auto my-2 my-lg-0"
+            style={{ maxHeight: "100px" }}
+            navbarScroll
+          >
+            <NavDropdown title="CATEGORIES" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="/reviews/category/strategy">
+                Strategy
+              </NavDropdown.Item>
+              <NavDropdown.Item href="/reviews/category/hidden-roles">
+                Hidden-roles
+              </NavDropdown.Item>
+              <NavDropdown.Item href="/reviews/category/dexterity">
+                Dexterity
+              </NavDropdown.Item>
+              <NavDropdown.Item href="/reviews/category/push-your-luck">
+                Push-your-luck
+              </NavDropdown.Item>
+              <NavDropdown.Item href="/reviews/category/roll-and-write">
+                Roll-and-write
+              </NavDropdown.Item>
+              <NavDropdown.Item href="/reviews/category/deck-building">
+                Deck-building
+              </NavDropdown.Item>
+              <NavDropdown.Item href="/reviews/category/engine-building">
+                Engine-building
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="/categories">
+                See All Categories
+              </NavDropdown.Item>
+            </NavDropdown>
+            {/* <Navbar.Text>Signed in as:</Navbar.Text> */}
+            <Nav.Link eventKey="disabled">
+              <UserProfile />
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
     </>
   );
