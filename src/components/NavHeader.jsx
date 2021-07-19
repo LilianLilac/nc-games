1e4f396c491f2eda320cff4784f2cc891a684a72
Monika Kaploniak <monikakaploniak@Monikas-MacBook-Pro.local>
2021-07-19 12:41:17 +0100

    refactored navbar

--- src/components/NavHeader.jsx
+++ src/components/NavHeader.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
 import { NavDropdown } from "react-bootstrap";
 import UserProfile from "./UserProfile";
 
@@ -8,48 +9,50 @@ const NavHeader = () => {
   return (
     <>
       <Navbar sticky="top" bg="light" justify variant="light" expand="lg">
-        <Navbar.Brand href="/home">NC Games</Navbar.Brand>
-        {/* <Nav className="mr-auto"> */}
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="mr-auto my-2 my-lg-0"
-            style={{ maxHeight: "100px" }}
-            navbarScroll
-          >
-            <NavDropdown title="CATEGORIES" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="/reviews/category/strategy">
-                Strategy
-              </NavDropdown.Item>
-              <NavDropdown.Item href="/reviews/category/hidden-roles">
-                Hidden-roles
-              </NavDropdown.Item>
-              <NavDropdown.Item href="/reviews/category/dexterity">
-                Dexterity
-              </NavDropdown.Item>
-              <NavDropdown.Item href="/reviews/category/push-your-luck">
-                Push-your-luck
-              </NavDropdown.Item>
-              <NavDropdown.Item href="/reviews/category/roll-and-write">
-                Roll-and-write
-              </NavDropdown.Item>
-              <NavDropdown.Item href="/reviews/category/deck-building">
-                Deck-building
-              </NavDropdown.Item>
-              <NavDropdown.Item href="/reviews/category/engine-building">
-                Engine-building
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="/categories">
-                See All Categories
-              </NavDropdown.Item>
-            </NavDropdown>
-            {/* <Navbar.Text>Signed in as:</Navbar.Text> */}
-            <Nav.Link eventKey="disabled">
-              <UserProfile />
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+        <Container>
+          <Navbar.Brand href="/home">NC Games</Navbar.Brand>
+          {/* <Nav className="mr-auto"> */}
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="mr-auto my-2 my-lg-0"
+              style={{ maxHeight: "100px" }}
+              navbarScroll
+            >
+              <NavDropdown title="CATEGORIES" id="collasible-nav-dropdown">
+                <NavDropdown.Item href="/reviews/category/strategy">
+                  Strategy
+                </NavDropdown.Item>
+                <NavDropdown.Item href="/reviews/category/hidden-roles">
+                  Hidden-roles
+                </NavDropdown.Item>
+                <NavDropdown.Item href="/reviews/category/dexterity">
+                  Dexterity
+                </NavDropdown.Item>
+                <NavDropdown.Item href="/reviews/category/push-your-luck">
+                  Push-your-luck
+                </NavDropdown.Item>
+                <NavDropdown.Item href="/reviews/category/roll-and-write">
+                  Roll-and-write
+                </NavDropdown.Item>
+                <NavDropdown.Item href="/reviews/category/deck-building">
+                  Deck-building
+                </NavDropdown.Item>
+                <NavDropdown.Item href="/reviews/category/engine-building">
+                  Engine-building
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="/categories">
+                  See All Categories
+                </NavDropdown.Item>
+              </NavDropdown>
+              {/* <Navbar.Text>Signed in as:</Navbar.Text> */}
+              <Nav.Link eventKey="disabled">
+                <UserProfile />
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
       </Navbar>
     </>
   );
